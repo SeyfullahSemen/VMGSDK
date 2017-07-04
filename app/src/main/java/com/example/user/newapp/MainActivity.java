cffd48b91025c774403092986296d9a597be3548
seyf <seyfo12@hotmail.com>
2017-07-04 15:19:27 +0200

    webview klapt uit na button klik

--- app/src/main/java/com/example/user/newapp/MainActivity.java
+++ app/src/main/java/com/example/user/newapp/MainActivity.java
@@ -1,36 +1,33 @@
 package com.example.user.newapp;
 
 /**
- * dit is de main activity dat is de plek waar de programma draait wanneer het programma opstart
+ * dit is de main activity dit is de plek waar de programma draait wanneer het programma opstart
  * dit is dus de plek waar de gebruiker van de app het eerste mee te maken zal hebben
  */
 // dit zijn de libarries die worden gebruikt
 
-import android.graphics.Color;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.MediaController;
-import android.widget.RatingBar;
 import android.widget.TextView;
 import android.webkit.WebViewClient;
-import android.widget.VideoView;
 
 
 public class MainActivity extends AppCompatActivity {
-    // dit zijn de variabelen die zijn aangemaakt
-    // je declareert ze private zodat niet alle klassen erbij kunnen komen en aanpassingen kunnen  maken aan deze variabelen
-
-    //    private Button btnCalc;
+    /**
+     * hier worden de variabelen gecreeered voor de app
+     * we hebben onder andere de webview, de text met lorem ipsum
+     * en de button die de video laad wanneer er op wordt geklikt
+     * de reden dat we ze private maken is zodat andere klassen de waarde van deze UI components niet kunnen aanpassen
+     * het zorgt voor encapsulatie
+     */
     private WebView webView;
     private TextView txtLorem;
+    private Button showWebView;
 
 
     /**
@@ -42,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         // wannneer er een android app is aangemaakt staan deze twee methodes er standaart in
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);// dit zorgt voor het inladen van de scherm
 
         /**
          * Je klikt en sleept componenten in je app en die krijgen allemaal een unieke ID mee om te werken met deze componenten
@@ -51,9 +48,20 @@ public class MainActivity extends AppCompatActivity {
 
         txtLorem = (TextView) findViewById(R.id.txtLorem); // lorem ipsum tekst
         webView = (WebView) findViewById(R.id.webViewTest); // de webview
+        showWebView = (Button) findViewById(R.id.showWebView);// de button om de webview te laten zien
+        /**
+         * hier beneden hebben we een event voor de button die ervoor zorgt dat de webview open gaat
+         */
+        showWebView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.setVisibility(View.VISIBLE);
+                setView();// hier roepen wij de methode aan die hier beneden is gemaakt
+            }
+        });
 
-     // roep de methode aan die wij hier beneden hebben gemaakt
-        setView();
+
+
     }
 
 
@@ -69,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
 
 //        // laad de url in de webview
 //        webView.loadUrl("http://support.adform.com/documentation/build-with-html5-studio/introduction/");
+        // maak een string aan met HTML codee erin
+        /**
+         * in deze string zit de code van:
+         * - de layout van de video
+         * - de formaat van de video
+         */
         String playVideo = "<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
@@ -87,27 +101,36 @@ public class MainActivity extends AppCompatActivity {
                 "    \n" +
                 "\n" +
                 "     </body>\n" +
-                "</html>";
+                "</html>";//einde van de HTML code
 
+        /**
+         * alles wat hier beneden gebeurd heeft te maken met het inladen van de video in de webview
+         * zoals de webview settings maken en de video laden nadat de pagina is geladen
+         *
+         */
         // maak een nieuwe instantie aan van de WebSettings klasse
-
-
         WebSettings webSettings = webView.getSettings();
 
         // gebruikt de methode SetJavaScriptEnabled() op true
         webSettings.setJavaScriptEnabled(true);
-
-
-
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         webSettings.setPluginState(WebSettings.PluginState.ON);
-
+        // maak een nieuwe instantie van de webviewclient
         webView.setWebViewClient(new WebViewClient() {
-            // autoplay when finished loading via javascript injection
-            public void onPageFinished(WebView view, String url) { webView.loadUrl("javascript:(function() { document.getElementsByTagName('video')[0].play(); })()"); }
-        });
-        webView.setWebChromeClient(new WebChromeClient());
 
+            /**
+             *
+             * @param view
+             * @param url
+             * autoplay gaat in ze werking zodra de pagina is geladen dit wordt gedaan via een javascript functie
+             */
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:(function() { document.getElementsByTagName('video')[0].play(); })()");
+            }
+        });
+        // maak een nieuwe instantie van de chrome webview client
+        webView.setWebChromeClient(new WebChromeClient());
+        // laad de datat in in de webview
         webView.loadData(playVideo, "text/html", "utf-8");
 
     }
