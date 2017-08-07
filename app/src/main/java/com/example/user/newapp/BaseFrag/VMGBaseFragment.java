1c592aa42bb4c8ba1810eadbaa0181d32d18a12a
seyf <seyfo12@hotmail.com>
2017-08-07 09:35:32 +0200

    nu wel

---
+++ app/src/main/java/com/example/user/newapp/BaseFrag/VMGBaseFragment.java
@@ -0,0 +1,101 @@
+package com.example.user.newapp.BaseFrag;
+
+import android.content.res.AssetManager;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+
+import com.example.user.newapp.EncodedBase;
+import com.example.user.newapp.Interfaces.JavascriptInterfaceVMG;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.example.user.newapp.R.id.webView;
+
+/**
+ * Created by Seyfullah Semen  on 4-8-2017.
+ */
+
+public abstract class VMGBaseFragment extends Fragment {
+    private String HTML;
+    private final String baseUrl = "http://vmg.host/";
+    private WebView webview;
+    private String mraidJs;
+
+    public VMGBaseFragment() {
+
+    }
+
+
+    public  void addJavascript(String javascript) {
+        addJavascript(webview, javascript);
+    }
+
+    public  void addJavascript(WebView webView, String javascript) {
+        if (!javascript.isEmpty()) {
+            webView.evaluateJavascript(javascript, new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String s) {
+                    Log.i("Info: ", "Evaluation done " + s);
+                }
+            });
+        } else {
+            System.out.println("Loading " + javascript);
+            webView.loadUrl("javascript:" + javascript);
+        }
+    }
+
+    public void addMraid(WebView webview) {
+        if (TextUtils.isEmpty(mraidJs)) {
+            byte[] mraidArray = Base64.decode(EncodedBase.mraidFile, Base64.DEFAULT);
+            mraidJs = new String(mraidArray);
+
+        }
+        System.out.println("adding mraid is oke and ready to go " + mraidJs.length());
+        webview.loadData("<html></html>", "text/html", "UTF-8");
+        webview.evaluateJavascript(mraidJs, new ValueCallback<String>() {
+            @Override
+            public void onReceiveValue(String s) {
+                Log.i("Info", "Mraiding" + mraidJs);
+            }
+        });
+        openWeb(baseUrl, HTML);
+
+    }
+
+    public void openWeb(String baseUrl, String HTML) {
+        AssetManager assetManager = getActivity().getAssets();
+        String text = "";
+        try {
+            InputStream input = assetManager.open(this.HTML);
+            int size = input.available();
+            byte[] buffer = new byte[size];
+            input.read(buffer);
+            input.close();
+
+            text = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.webview.loadDataWithBaseURL(baseUrl, text, "text/html", "UTF-8", "");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+}
+
+
